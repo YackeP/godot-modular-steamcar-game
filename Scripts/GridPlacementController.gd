b66957e64d0afe8890803be56efa0fa43697f2aa
Jacek Pietracha <pietracha.jacek@gmail.com>
2023-10-24 01:33:27 +0200

    Instantiate a scene on a coordinate grid

--- Scripts/GridPlacementController.gd
+++ Scripts/GridPlacementController.gd
@@ -2,8 +2,8 @@ extends Node3D
 
 class_name GridPlacementController
 
-var house_1 = load("res://Scenes/House1.tscn") # TODO: figure out the typing for this
-var house_2 = load("res://Scenes/House2.tscn") # TODO: figure out the typing for this
+var house_1: PackedScene = preload("res://Scenes/House1.tscn") # TODO: figure out the typing for this
+var house_2: PackedScene = preload("res://Scenes/House2.tscn") # TODO: figure out the typing for this
 
 const ray_length = 1000
 
@@ -18,6 +18,12 @@ func _input(event):
 		var to = from + camera.project_ray_normal(event.position) * ray_length
 		var space_state = get_world_3d().direct_space_state
 		# use global coordinates, not local to node
-		var result = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to))
-		print(result)
+		var result: Dictionary = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to))
+		
+		if !result.is_empty():
+			var result_pos = result['position']
+			var house_1_inst: Node3D = house_1.instantiate()
+			add_child(house_1_inst)
+			house_1_inst.position = Vector3(round(result_pos.x), result_pos.y, round(result_pos.z))
+			print(result)
 		
