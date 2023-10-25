08af3ca6548b4d84fe42eb7c98859104bb6a5393
Jacek Pietracha <pietracha.jacek@gmail.com>
2023-10-25 23:52:08 +0200

    Small refactorings

--- Scripts/GridPlacementController.gd
+++ Scripts/GridPlacementController.gd
@@ -8,55 +8,71 @@ var houseScene2: PackedScene = preload("res://Scenes/House2.tscn")
 var houseGhostScene1: PackedScene = preload("res://Scenes/House1Ghost.tscn")
 var houseGhostScene2: PackedScene = preload("res://Scenes/House2Ghost.tscn")
 
-var selectedHouse: PackedScene
+var selectedHouseScene: PackedScene
 
 var houseGhost: Node3D
 
 const ray_length = 1000
 
 func _physics_process(delta) -> void:
-	if houseGhost != null:
-		houseGhost.queue_free()
-		remove_child(houseGhost)
-	if selectedHouse != null:
-		var camera = get_viewport().get_camera_3d()
-		var mousePosition = get_viewport().get_mouse_position()
-		var from = camera.project_ray_origin(mousePosition)
-		var to = from + camera.project_ray_normal(mousePosition) * ray_length
-		var space_state = get_world_3d().direct_space_state
-		# use global coordinates, not local to node
-		var result: Dictionary = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to))
+	if selectedHouseScene != null:
+		var result: Dictionary = getObjectHitByMouse()
 		
 		if !result.is_empty():
 			var result_pos = result['position']
+			if houseGhost == null:
+				createGhost(houseGhostScene1 if selectedHouseScene == houseScene1 else houseGhostScene2)
 			
-			if selectedHouse == houseScene1:
-				houseGhost = houseGhostScene1.instantiate()
-			elif selectedHouse == houseScene2:
-				houseGhost = houseGhostScene2.instantiate()
-			add_child(houseGhost)
-			houseGhost.position = Vector3(round(result_pos.x), result_pos.y, round(result_pos.z))
+			houseGhost.position = getNearestGridPosition(result_pos)
+		elif houseGhost != null:
+			removeGhost()
+
 
 func _input(event):
 	if event is InputEventKey:
 		if event.keycode == KEY_1:
-			selectedHouse = houseScene1
+			selectedHouseScene = houseScene1
+			removeGhost()
+			createGhost(houseGhostScene1)
 		elif event.keycode == KEY_2:
-			selectedHouse = houseScene2
+			selectedHouseScene = houseScene2
+			removeGhost()
+			createGhost(houseGhostScene2)
 	
-	elif event is InputEventMouseButton and event.pressed and event.button_index == 1 and selectedHouse != null:
+	elif event is InputEventMouseButton and event.pressed and event.button_index == 1 and selectedHouseScene != null:
 		print("Clicked button")
-		var camera = get_viewport().get_camera_3d()
-		var from = camera.project_ray_origin(event.position)
-		var to = from + camera.project_ray_normal(event.position) * ray_length
-		var space_state = get_world_3d().direct_space_state
-		# use global coordinates, not local to node
-		var result: Dictionary = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to))
+		var result: Dictionary = getObjectHitByMouse()
 		
 		if !result.is_empty():
 			var result_pos = result['position']
-			var house1Inst: Node3D = selectedHouse.instantiate()
-			add_child(house1Inst)
-			house1Inst.position = Vector3(round(result_pos.x), result_pos.y, round(result_pos.z))
+			createGridObject(selectedHouseScene, result_pos)
 			print(result)
 
+
+func removeGhost() -> void:
+	if( houseGhost != null):
+		houseGhost.queue_free()
+		remove_child(houseGhost)
+
+func createGhost(ghost: PackedScene) -> void:
+	var ghostInstance = ghost.instantiate()
+	houseGhost = ghostInstance
+	add_child(ghostInstance)
+	
+func createGridObject(object: PackedScene, pos: Vector3) -> void:
+	var objectInstance = object.instantiate()
+	add_child(objectInstance)
+	objectInstance.position = getNearestGridPosition(pos)
+
+func getNearestGridPosition(rawPosition: Vector3) -> Vector3:
+	return Vector3(round(rawPosition.x), rawPosition.y, round(rawPosition.z))
+	
+# this can get extracted to some sort of a util class
+func getObjectHitByMouse() -> Dictionary:
+	var camera = get_viewport().get_camera_3d()
+	var mousePos = get_viewport().get_mouse_position()
+	var from = camera.project_ray_origin(mousePos)
+	var to = from + camera.project_ray_normal(mousePos) * ray_length
+	var space_state = get_world_3d().direct_space_state
+	return space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to)) 
+	
