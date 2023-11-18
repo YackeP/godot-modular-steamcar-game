702cf2bae7034119300a201b53b6901807f25fbd
Jacek Pietracha <pietracha.jacek@gmail.com>
2023-11-18 18:35:18 +0100

    Add a simple debug text system

--- Scripts/DebugNode.gd
+++ Scripts/DebugNode.gd
@@ -2,7 +2,21 @@ extends Node3D
 
 class_name DebugNode
 
+var textValueGetters: Array[Callable]
+
+@onready var textLabel = $Text as Label3D
+
 func _enter_tree():
 	var debugSettings: DebugSettings = get_node("/root/DebugSettings")
 	if !debugSettings.SHOW_DEBUG_NODES:
 		hide()
+
+
+func addTextValueGetter(getter: Callable) -> void:
+	textValueGetters.append(getter)
+	
+func _physics_process(delta: float) -> void:
+	var text = ""
+	for getter in textValueGetters:
+		text += getter.call() + "\n"
+	textLabel.text = text
