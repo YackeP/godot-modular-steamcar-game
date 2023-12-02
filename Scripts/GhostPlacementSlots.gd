8dedb676a9cb56f861dc2d2d5e9c93d6b76eef17
Jacek Pietracha <pietracha.jacek@gmail.com>
2023-12-02 23:14:51 +0100

    Fix a bug when placing a grid component would take some other grid space

--- Scripts/GhostPlacementSlots.gd
+++ Scripts/GhostPlacementSlots.gd
@@ -5,14 +5,18 @@ class_name GhostPlacementSlots
 func overlapsAllSlots() -> bool:
 	Logger.info("Ghost is overlappingBodies:",get_overlapping_bodies().reduce(func(acc: String, body:Node3D): return acc + ", " + body.name,""))
 	var gridSize = get_child_count()
-	return get_overlapping_bodies().filter(_bodyIsGridSpace).size() == gridSize
+	var DEBUG_overlapping_bodies = _getOverlappingGridSpaces()
+	return _getOverlappingGridSpaces().size() == gridSize
 
 func getAllOverlappedSlots() -> Array[GridSpace]:
-	var tempArr: Array[GridSpace]
-	tempArr.assign(get_overlapping_bodies().filter(_bodyIsGridSpace))
-#(gridArea.get_overlapping_bodies() as Array[GridSpace]).filter(func(body:GridSpace): return body is GridSpace) as Array[GridSpace] -- this won't work, since "filter() will allways returns an untyped array"
-# https://www.reddit.com/r/godot/comments/12n4pfc/gdscript_how_to_use_typed_arrays_with_lambda/
-	return tempArr
+	return _getOverlappingGridSpaces()
 
 func _bodyIsGridSpace(body: Node3D) -> bool:
 	return body is GridSpace
+	
+func _getOverlappingGridSpaces() -> Array[GridSpace]:
+	#(gridArea.get_overlapping_bodies() as Array[GridSpace]).filter(func(body:GridSpace): return body is GridSpace) as Array[GridSpace] -- this won't work, since "filter() will allways returns an untyped array"
+# https://www.reddit.com/r/godot/comments/12n4pfc/gdscript_how_to_use_typed_arrays_with_lambda/
+	var tempArr: Array[GridSpace]
+	tempArr.assign(get_overlapping_bodies().filter(_bodyIsGridSpace))
+	return tempArr
